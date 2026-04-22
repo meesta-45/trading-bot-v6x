cbc473ed316e625ae95440e1ad5a1970be56c69f
meesta-45 <andrew254mwash@gmail.com>
2026-04-22 16:37:05 +0100

    Create execution.py

---
+++ bot/execution.py
@@ -0,0 +1,17 @@
+import random
+from config import MODE
+
+class ExecutionEngine:
+
+    def execute(self, symbol, signal):
+
+        if MODE == "PAPER":
+            return random.choice(["WIN", "LOSS"])
+
+        elif MODE == "DEMO":
+            print(f"[DEMO TRADE] {symbol} {signal}")
+            return "WIN"
+
+        elif MODE == "LIVE":
+            print(f"[LIVE TRADE READY] {symbol} {signal}")
+            return "PENDING"
