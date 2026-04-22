7aac83c3f187e004f4981f3aad2181acbef2e1f3
meesta-45 <andrew254mwash@gmail.com>
2026-04-22 16:38:28 +0100

    Create engine.py

---
+++ bot/engine.py
@@ -0,0 +1,32 @@
+from strategy import Strategy
+from risk import RiskManager
+from execution import ExecutionEngine
+from config import SYMBOL
+
+class Engine:
+
+    def __init__(self):
+        self.prices = []
+        self.strategy = Strategy()
+        self.risk = RiskManager()
+        self.exec = ExecutionEngine()
+
+    def on_price(self, price):
+
+        self.prices.append(price)
+
+        if len(self.prices) > 100:
+            self.prices.pop(0)
+
+        if len(self.prices) < 30:
+            return
+
+        signal = self.strategy.signal(self.prices)
+
+        if self.risk.allow(signal):
+
+            result = self.exec.execute(SYMBOL, signal)
+
+            self.risk.update(result)
+
+            print("SIGNAL:", signal, "RESULT:", result)
