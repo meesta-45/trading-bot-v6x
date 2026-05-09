482b3f5bf67fba3a9e6dd12a5c1a2bb2590a35b4
meesta-45 <andrew254mwash@gmail.com>
2026-05-09 14:49:57 +0100

    Update feed.py

--- bot/feed.py
+++ bot/feed.py
@@ -8,17 +8,30 @@ class Feed:
         self.callback = callback
 
     def on_message(self, ws, message):
+
         data = json.loads(message)
 
         if "tick" in data:
             price = float(data["tick"]["quote"])
+
+            print("LIVE PRICE:", price)
+
             self.callback(price)
 
+    def on_open(self, ws):
+
+        payload = {
+            "ticks": self.symbol
+        }
+
+        ws.send(json.dumps(payload))
+
     def start(self):
+
         ws = websocket.WebSocketApp(
             "wss://ws.binaryws.com/websockets/v3?app_id=1089",
-            on_message=self.on_message
+            on_message=self.on_message,
+            on_open=self.on_open
         )
 
-        ws.send(json.dumps({"ticks": self.symbol}))
         ws.run_forever()
