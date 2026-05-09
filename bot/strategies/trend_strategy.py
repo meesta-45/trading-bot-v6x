fe35a4c0701a6307f83860c188d4dac4760369d7
meesta-45 <andrew254mwash@gmail.com>
2026-05-09 16:05:17 +0100

    Update trend_strategy.py

--- bot/strategies/trend_strategy.py
+++ bot/strategies/trend_strategy.py
@@ -1,1 +1,18 @@
+class TrendStrategy:
 
+    def analyze(self, prices):
+
+        if len(prices) < 30:
+            return ("NO_TRADE", 0)
+
+        short = sum(prices[-5:]) / 5
+        mid = sum(prices[-15:]) / 15
+        long = sum(prices[-30:]) / 30
+
+        if short > mid > long:
+            return ("RISE", 80)
+
+        if short < mid < long:
+            return ("FALL", 80)
+
+        return ("NO_TRADE", 0)
