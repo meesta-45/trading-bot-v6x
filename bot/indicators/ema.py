dc224f3c0ba699d267a43f39e7055c141a9e582c
meesta-45 <andrew254mwash@gmail.com>
2026-05-30 11:24:05 +0100

    Update ema.py

--- bot/indicators/ema.py
+++ bot/indicators/ema.py
@@ -1,1 +1,14 @@
+def calculate_ema(prices, period):
 
+    if len(prices) < period:
+        return None
+
+    multiplier = 2 / (period + 1)
+
+    ema = sum(prices[:period]) / period
+
+    for price in prices[period:]:
+
+        ema = (price - ema) * multiplier + ema
+
+    return ema
