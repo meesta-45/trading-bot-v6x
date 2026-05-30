d565df9dc04b14c24236122b4a44341a8696d2eb
meesta-45 <andrew254mwash@gmail.com>
2026-05-30 11:30:12 +0100

    Update confidence.py

--- bot/confidence.py
+++ bot/confidence.py
@@ -1,1 +1,25 @@
+class ConfidenceEngine:
 
+    def calculate(
+        self,
+        trend,
+        macd,
+        adx,
+        momentum
+    ):
+
+        score = 0
+
+        if trend:
+            score += 25
+
+        if macd:
+            score += 25
+
+        if adx:
+            score += 25
+
+        if momentum:
+            score += 25
+
+        return score
